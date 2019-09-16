a629b89edfe437b0ad7c91a459ab321eeafd8fb7
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-16 11:21:21 +0200

    Import and add App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello react-router</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, 
+	document.getElementById('root'));
 
 serviceWorker.unregister();
