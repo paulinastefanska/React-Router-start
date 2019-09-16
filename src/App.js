e75404b9f2c06dda9e6d1f276f44f97a859a989d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-16 11:58:54 +0200

    Add components and route parameters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+
+const Navigation = props => (
+    <div>
+       <ul>
+            <li><Link to="/" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Home</Link></li>
+            <li><Link to="/contact" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Contact</Link></li>
+        </ul>
+        {props.children}
+    </div>
+);
+const Home = () => <h1>Hej, tu Home component</h1>;
+const Contact = () => <h1>A tu Contact component</h1>;
+const PageNotFound = () => <h1>404 Not Found</h1>;
+const Hello = (props) => <h1>Witaj, {props.params.name}</h1>
+
 class App extends Component {
  render() {
    return (
-     <Router history={hashHistory}>
-       <Route path='/' component={Home} />
-       <Route path='/contact' component={Contact} />
-     </Router>
+    <Router history={hashHistory}>
+	    <Route path='/' component={Navigation}>
+	        <IndexRoute component={Home} />
+	        <Route path='/contact' component={Contact} />
+	        <Route path='/hello/:name' component={Hello} />
+	        <Route path='*' component={PageNotFound} />
+	    </Route>
+	</Router>
    )
  }
-}
-const Home = () => <h1>Hej, tu Home component</h1>
-const Contact = () => <h1>A tu Contact component</h1>
+};
+
 export default App
